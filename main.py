e69912c4717af11c211d58c66739851b5679e831
rzeraat.tur <rzeraat.tur@gmail.com>
2023-08-27 11:30:15 +0300

    refactor data processing

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import os
 import pandas as pd
 from src.utils.fetch_bitcoin_data import fetch_latest_bitcoin_data
-from src.data_preprocessing.preprocess_data import preprocess_bitcoin_data
+from src.data_preprocessing.preprocess_data import BitcoinPreprocessor
 from src.eda.bitcoin_eda import perform_eda
-from src.feature_engineering.feature_engineering import feature_engineering
 from src.models.train import load_data, train_models, evaluate_models, ensemble_predictions, scale_features, predict_next_day
 from sklearn.metrics import mean_squared_error
-from src.data_preprocessing.clean_data import clean_data
 
 # Configuration
 RAW_DATA_PATH = os.path.join('data', 'raw', 'bitcoin_data.csv')
 PROCESSED_DATA_PATH = os.path.join('data', 'processed', 'processed_bitcoin_data.csv')
-ENGINEERED_DATA_PATH = os.path.join('data', 'processed', 'engineered_bitcoin_data.csv')
 API_KEY = '402db71a-cbac-4ff7-8120-2053b050d2ae'
 
 def fetch_data():
@@ -21,29 +18,16 @@ def fetch_data():
 
 def preprocess():
     print("Preprocessing data...")
-    preprocess_bitcoin_data(RAW_DATA_PATH, PROCESSED_DATA_PATH)
+    preprocessor = BitcoinPreprocessor(RAW_DATA_PATH, PROCESSED_DATA_PATH)
+    preprocessor.preprocess()
     print("Data preprocessed successfully!")
-
-def clean():
-    df = pd.read_csv(PROCESSED_DATA_PATH)
-    print("Cleaning data...")
-    df = clean_data(df)
-    df.to_csv(PROCESSED_DATA_PATH, index=False)
-    print("Data cleaned successfully!")
-
-def engineer_features():
-    df = pd.read_csv(PROCESSED_DATA_PATH)
-    print("Performing Feature Engineering...")
-    df = feature_engineering(df)
-    df.to_csv(ENGINEERED_DATA_PATH, index=False)
-    print("Feature Engineering completed successfully!")
 
 def perform_analysis():
     print("Performing Exploratory Data Analysis (EDA)...")
-    perform_eda(ENGINEERED_DATA_PATH)
+    perform_eda(PROCESSED_DATA_PATH)
 
 def train_and_evaluate():
-    X_train, X_test, y_train, y_test = load_data(ENGINEERED_DATA_PATH)
+    X_train, X_test, y_train, y_test = load_data(PROCESSED_DATA_PATH)
     X_train, X_test = scale_features(X_train, X_test)
     trained_models = train_models(X_train, y_train, incremental=True)
     model_predictions = evaluate_models(trained_models, X_train ,X_test)
@@ -53,7 +37,7 @@ def train_and_evaluate():
     return trained_models
 
 def predict(trained_models):
-    df = pd.read_csv(ENGINEERED_DATA_PATH)
+    df = pd.read_csv(PROCESSED_DATA_PATH)
     prediction = predict_next_day(trained_models, df)
     for i, pred in enumerate(prediction):
         print(f"Ensemble predicted Bitcoin price for day {i+1}: ${pred:.2f}")
@@ -61,8 +45,6 @@ def predict(trained_models):
 def main():
     fetch_data()
     preprocess()
-    clean()
-    engineer_features()
     perform_analysis()
     trained_models = train_and_evaluate()
     predict(trained_models)
--- src/data_preprocessing/preprocess_data.py
+++ src/data_preprocessing/preprocess_data.py
@@ -1,56 +1,122 @@
 import pandas as pd
+import numpy as np
 
-def fill_missing_values(data):
-    """Fill missing values in the dataset."""
-    data.fillna(method='ffill', inplace=True)
-    data['tvl'].fillna(0, inplace=True)
-    return data
+class BitcoinPreprocessor:
+    def __init__(self, input_path, output_path):
+        self.data = pd.read_csv(input_path)
+        self.output_path = output_path
 
-def compute_moving_average(data, window=7):
-    """Compute the moving average for the Bitcoin price."""
-    data['7_day_avg'] = data['price'].rolling(window=window).mean()
-    data['30_day_avg'] = data['price'].rolling(window=30).mean()
+    def fill_missing_values(self):
+        self.data.fillna(method='ffill', inplace=True)
+        self.data['tvl'].fillna(0, inplace=True)
 
-    return data
+    def compute_moving_average(self, window=7):
+        self.data['7_day_avg'] = self.data['price'].rolling(window=window).mean()
+        self.data['30_day_avg'] = self.data['price'].rolling(window=30).mean()
 
+    def convert_data_types(self):
+        numerical_cols = ['price', 'volume_24h', 'volume_change_24h', 'percent_change_1h', 'percent_change_24h', 
+                          'percent_change_7d', 'percent_change_30d', 'percent_change_60d', 'percent_change_90d', 
+                          'market_cap', 'market_cap_dominance', 'fully_diluted_market_cap', 'tvl']
+        for col in numerical_cols:
+            self.data[col] = self.data[col].astype(float)
+        self.data['last_updated'] = pd.to_datetime(self.data['last_updated'], errors='coerce')
 
-def convert_data_types(data):
-    """Convert columns to appropriate data types."""
-    numerical_cols = ['price', 'volume_24h', 'volume_change_24h', 'percent_change_1h', 'percent_change_24h', 
-                      'percent_change_7d', 'percent_change_30d', 'percent_change_60d', 'percent_change_90d', 
-                      'market_cap', 'market_cap_dominance', 'fully_diluted_market_cap', 'tvl']
-    
-    # Convert numerical columns to float
-    for col in numerical_cols:
-        data[col] = data[col].astype(float)
-    
-    # Convert last_updated to datetime
-    data['last_updated'] = pd.to_datetime(data['last_updated'], errors='coerce')
-    
-    return data
+    def extract_date_features(self):
+        self.data['date'] = self.data['last_updated'].dt.date
+        self.data['day'] = self.data['last_updated'].dt.day
+        self.data['month'] = self.data['last_updated'].dt.month
+        self.data['year'] = self.data['last_updated'].dt.year
 
+    def feature_engineering(self):
+        # Convert 'date' column to Unix timestamp
+        if 'date' in self.data.columns:
+            self.data['date'] = pd.to_datetime(self.data['date']).astype(int) / 10**9
 
-def extract_date_features(data):
-    """Extract date-related features from the dataset."""
-    data['date'] = data['last_updated'].dt.date
-    data['day'] = data['last_updated'].dt.day
-    data['month'] = data['last_updated'].dt.month
-    data['year'] = data['last_updated'].dt.year
-    return data
+        # Convert 'last_updated' column to Unix timestamp
+        if 'last_updated' in self.data.columns:
+            self.data['last_updated'] = pd.to_datetime(self.data['last_updated']).astype(int) / 10**9
 
-def preprocess_bitcoin_data(input_path='data/raw/bitcoin_data.csv', output_path='data/processed/processed_bitcoin_data.csv'):
-    # Load the raw data
-    data = pd.read_csv(input_path)
+        # Date and Time Features
+        self.data['day_of_week'] = pd.to_datetime(self.data['date'], unit='s').dt.dayofweek
+        self.data['month'] = pd.to_datetime(self.data['date'], unit='s').dt.month
+        self.data['year'] = pd.to_datetime(self.data['date'], unit='s').dt.year
+        self.data['is_weekend'] = self.data['day_of_week'].apply(lambda x: 1 if x >= 5 else 0)
 
-    # Preprocessing steps
-    data = fill_missing_values(data)
-    data = convert_data_types(data)
-    data = extract_date_features(data)
-    data = compute_moving_average(data)  # Add this line
+        # Lagged Features
+        for lag in range(1, 8):  # for one week
+            self.data[f'price_lag_{lag}'] = self.data['price'].shift(lag)
 
-    # Save the processed data
-    data.to_csv(output_path, index=False)
-    print(f"Data preprocessed and saved to {output_path}!")
+        # Rolling Window Statistics
+        self.data['rolling_mean_7'] = self.data['price'].rolling(window=7).mean()
+        self.data['rolling_std_7'] = self.data['price'].rolling(window=7).std()
+        self.data['rolling_min_7'] = self.data['price'].rolling(window=7).min()
+        self.data['rolling_max_7'] = self.data['price'].rolling(window=7).max()
+
+        # Differences and Returns
+        self.data['daily_return'] = self.data['price'].pct_change()
+        self.data['log_return'] = np.log(self.data['price']).diff()
+
+        # Exponential Weighted Features
+        self.data['ewm_7'] = self.data['price'].ewm(span=7).mean()
+
+        # Handle missing values
+        self.data.fillna(method='ffill', inplace=True)  # Forward fill to handle missing values
+        self.data.fillna(method='bfill', inplace=True)  # Backward fill for any remaining missing values
+
+    def clean_data(self):
+        # Convert date columns to datetime data type
+        self.data['last_updated'] = pd.to_datetime(self.data['last_updated'])
+        self.data['date'] = pd.to_datetime(self.data['date'])
+        
+        # Calculate the 30-day rolling average for the 'price' column
+        self.data['30_day_avg'] = self.data['price'].rolling(window=30, min_periods=1).mean()
+        
+        # Handle missing values for all columns
+        for column in self.data.columns:
+            # If the column is of numeric type, fill missing values with forward fill and then backward fill
+            if pd.api.types.is_numeric_dtype(self.data[column]):
+                self.data[column].fillna(method='ffill', inplace=True)
+                self.data[column].fillna(method='bfill', inplace=True)
+            # If the column is of object type (like strings), fill missing values with the mode (most frequent value)
+            elif pd.api.types.is_object_dtype(self.data[column]):
+                mode_value = self.data[column].mode()[0]
+                self.data[column].fillna(value=mode_value, inplace=True)
+            # If the column is of datetime type, fill missing values with the most recent date
+            elif pd.api.types.is_datetime64_any_dtype(self.data[column]):
+                self.data[column].fillna(method='ffill', inplace=True)
+                self.data[column].fillna(method='bfill', inplace=True)
+
+    def addDateTimeFeatures(self):
+        # Additional Date Features
+        self.data['hour'] = pd.to_datetime(self.data['last_updated'], unit='s').dt.hour
+        self.data['minute'] = pd.to_datetime(self.data['last_updated'], unit='s').dt.minute
+        self.data['second'] = pd.to_datetime(self.data['last_updated'], unit='s').dt.second
+
+        # Cyclic encoding for hour, minute, and second
+        self.data['hour_sin'] = np.sin(2 * np.pi * self.data['hour'] / 24)
+        self.data['hour_cos'] = np.cos(2 * np.pi * self.data['hour'] / 24)
+        self.data['minute_sin'] = np.sin(2 * np.pi * self.data['minute'] / 60)
+        self.data['minute_cos'] = np.cos(2 * np.pi * self.data['minute'] / 60)
+        self.data['second_sin'] = np.sin(2 * np.pi * self.data['second'] / 60)
+        self.data['second_cos'] = np.cos(2 * np.pi * self.data['second'] / 60)
+        self.data['datetime'] = pd.to_datetime(self.data[['year', 'month', 'day', 'hour', 'minute', 'second']])
+        self.data.set_index('datetime', inplace=True)
+        
+    def preprocess(self):
+        self.fill_missing_values()
+        self.convert_data_types()
+        self.extract_date_features()
+        self.compute_moving_average()
+        self.clean_data()
+        self.feature_engineering()
+        self.addDateTimeFeatures()
+        self.data.to_csv(self.output_path, index=False)
+        print(f"Data preprocessed and saved to {self.output_path}!")
 
 if __name__ == "__main__":
-    preprocess_bitcoin_data()
+    INPUT_PATH = 'data/raw/bitcoin_data.csv'
+    OUTPUT_PATH = 'data/processed/processed_bitcoin_data.csv'
+    preprocessor = BitcoinPreprocessor(INPUT_PATH, OUTPUT_PATH)
+    preprocessor.preprocess()
+
